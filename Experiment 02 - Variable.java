85730bce4a24cf50220b942fac6e1af0f52744f5
Jefin Joseph J <140164882+jefinjosephj@users.noreply.github.com>
2024-01-29 15:59:15 +0530

    Add files via upload

---
+++ Experiment 02 - Variable.java
@@ -0,0 +1,18 @@
+class demo{
+    int x,y;
+}
+public class variable{
+    public static void main(String[] args) {
+        int sum1,sum2;
+        demo obj1 = new demo();
+        demo obj2 = new demo();
+        obj1.x=10;
+        obj1.y=20;
+        obj2.x=30;
+        obj2.y=40;
+        sum1 = obj1.x+obj1.y;
+        sum2 = obj2.x+obj2.y;
+        System.out.println("Sum = "+sum1);
+        System.out.println("Sum = "+sum2);
+    }
+}
